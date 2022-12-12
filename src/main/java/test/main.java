d796a895acf52e45860859a1216bd46ca70dc9b6
yuzuwxy <w1149639121@163.com>
2022-12-12 22:28:20 +0800

    create by wxy

---
+++ src/main/java/test/main.java
@@ -0,0 +1,11 @@
+/**
+ * @BelongsProject: database_keshe
+ * @BelongsPackage: test
+ * @ClassName:main
+ * @Author: yuzuwxy
+ * @CreateTime: 2022-12-12  22:27
+ */
+package test;
+
+public class main {
+}
