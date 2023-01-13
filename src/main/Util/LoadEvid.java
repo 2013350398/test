ecb7f6f78d3a1304d7989ac60d4f6ca19dea51b1
yuzuwxy <w1149639121@163.com>
2023-01-14 03:17:39 +0800

    文件上传下载（刚刚忘了

--- src/main/Util/LoadEvid.java
+++ src/main/Util/LoadEvid.java
@@ -5,15 +5,27 @@ import java.io.*;
 
 public class LoadEvid {
     public static String uploadEvid(String localPath) {
-        String serverPath = "";
-
         try {
             File source = new File(localPath);
             FileInputStream fis = new FileInputStream(source);
-
-            File dfile = new File("./evidence" + source.getName());
+            filename = new StringBuffer("./evidence/" + source.getName());
+            File dfile = new File(filename.toString());
+            File pafile=dfile.getParentFile();
+            //不存在父路径就创建
+            if(!pafile.exists())
+                pafile.mkdirs();
             if(!dfile.exists())
                 dfile.createNewFile();
+            else
+                while(dfile.exists()) {
+                    filename = filename.insert(filename.indexOf(".", 1), "(1)") ;
+                    dfile.renameTo(new File(filename.toString()));
+                    //                System.out.println(filename + "" + dfile.exists());
+                    if(!dfile.exists()){
+                        dfile.createNewFile();
+                        break;
+                    }
+                }
 
             FileOutputStream fos = new FileOutputStream(dfile);
             byte[] b = new byte[1024];
@@ -29,15 +41,17 @@ public class LoadEvid {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return serverPath;
+        return filename;
     }
 
     public static String downloadEvid(String serverPath) {
         try {
+            Map<String, String> map = System.getenv();
+            String userName = map.get("USERNAME");
             File source = new File(serverPath);
             FileInputStream fis = new FileInputStream(source);
 
-            File dfile = new File("C://Download//" + source.getName());
+            File dfile = new File("C://Users//" + userName + "//Downloads//" + source.getName());
             if(!dfile.exists())
                 dfile.createNewFile();
 
